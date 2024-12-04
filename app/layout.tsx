fdbd1275d99b3b5147300f3116161388726e19c9
Jason Desiderio <jason@thegoodfornothings.club>
2024-12-04 11:47:53 -0800

    Add fonts, colors, and logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,21 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Alegreya, Outfit } from 'next/font/google'
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+const outfit = Outfit({
+  subsets: ['latin'],
+  display: 'swap',
+  weight: 'variable',
+  style: ['normal'],
+  variable: '--font-outfit',
 });
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+
+const alegreya = Alegreya({
+  subsets: ['latin'],
+  display: 'swap',
+  weight: 'variable',
+  style: ['normal', 'italic'],
+  variable: '--font-alegreya',
 });
 
 export const metadata: Metadata = {
@@ -26,7 +31,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${outfit.variable} ${alegreya.variable} antialiased`}
       >
         {children}
       </body>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,8 +10,8 @@ export default {
   theme: {
   	extend: {
   		colors: {
-  			background: 'hsl(var(--background))',
-  			foreground: 'hsl(var(--foreground))',
+  			background: 'var(--background)',
+  			foreground: 'var(--foreground)',
   			card: {
   				DEFAULT: 'hsl(var(--card))',
   				foreground: 'hsl(var(--card-foreground))'
