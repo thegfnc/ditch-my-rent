780542cca4051a84e562231581c85ac75cdbf5ce
Jason Desiderio <jason@thegoodfornothings.club>
2024-12-04 17:21:19 -0800

    Make names more generic

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,40 +1,41 @@
-import type { Metadata } from "next";
+import type { Metadata } from 'next'
 import { Alegreya, Outfit } from 'next/font/google'
-import "./globals.css";
+import './globals.css'
 
-const outfit = Outfit({
+const fontSans = Outfit({
   subsets: ['latin'],
   display: 'swap',
   weight: 'variable',
   style: ['normal'],
-  variable: '--font-outfit',
-});
+  variable: '--font-sans',
+})
 
-const alegreya = Alegreya({
+const fontSerif = Alegreya({
   subsets: ['latin'],
   display: 'swap',
   weight: 'variable',
   style: ['normal', 'italic'],
-  variable: '--font-alegreya',
-});
+  variable: '--font-serif',
+})
 
 export const metadata: Metadata = {
-  title: "Ditch My Rent",
-  description: "Learn about the tools and resources available to help you ditch your rent.",
-};
+  title: 'Ditch My Rent',
+  description:
+    'Learn about the tools and resources available to help you ditch your rent.',
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body
-        className={`${outfit.variable} ${alegreya.variable} antialiased`}
+        className={`${fontSans.variable} ${fontSerif.variable} antialiased`}
       >
         {children}
       </body>
     </html>
-  );
+  )
 }
