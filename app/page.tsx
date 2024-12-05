25b3ed7fdb651c99940f155918f4d805d9941d00
Jason Desiderio <jason@thegoodfornothings.club>
2024-12-04 17:18:46 -0800

    Add theme styles + prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,21 @@
-import Image from "next/image";
+import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <div className='grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 sm:p-20'>
+      <main className='row-start-2 flex flex-col items-center gap-8 sm:items-start'>
         <Image
-          className="dark:invert"
-          src="/ditch-my-rent__full-logo.svg"
-          alt="Ditch My Rent logo"
+          className='dark:invert'
+          src='/ditch-my-rent__full-logo.svg'
+          alt='Ditch My Rent logo'
           width={500}
           height={500}
           priority
         />
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+      <footer className='row-start-3 flex flex-wrap items-center justify-center gap-6'>
         Coming Soon
       </footer>
     </div>
-  );
+  )
 }
