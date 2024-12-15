1a20e8eff89e75875452ecf6749dea6bb4b3bcf0
Jason Desiderio <jason@thegoodfornothings.club>
2024-12-14 22:00:14 -0600

    Squish homepage a bit

--- app/page.tsx
+++ app/page.tsx
@@ -14,11 +14,11 @@ export default function Home() {
           height={360}
           priority
         />
-        <article className='prose-lg mx-auto max-w-[40ch] lg:prose-xl prose-h2:text-[24px] lg:prose-h2:text-[30px]'>
+        <article className='prose-lg mx-auto max-w-[32ch] lg:prose-xl prose-h2:text-[24px] lg:prose-h2:text-[30px]'>
           <h2 className='text-balance leading-tight'>
             America has a rent problem, and we&apos;re here to help fix it.
           </h2>
-          <p className='leading-snug'>
+          <p className='text-balance font-serif text-base leading-snug'>
             Nearly half of American households are burdened by rent, spending
             more than 30% of their income just to keep a roof over their heads.
             We believe everyone deserves a home that brings them happiness and
