087ba27bb559595c39413ce65825bb3ec0515159
Jason Desiderio <jason@thegoodfornothings.club>
2024-12-15 22:04:10 -0600

    Add microsoft clarity code

--- app/layout.tsx
+++ app/layout.tsx
@@ -52,6 +52,15 @@ export default async function RootLayout({
             gtag('config', 'G-WSJDKKNT29');
           `}
         </Script>
+        <Script id='microsoft-clarity'>
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "peovvps3vg");
+          `}
+        </Script>
       </body>
     </html>
   )
