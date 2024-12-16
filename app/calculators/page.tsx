162775f6269281c821fbdd821ad7c26d08b69687
Jason Desiderio <jason@thegoodfornothings.club>
2024-12-15 21:45:16 -0600

    Add a boolean to hide

--- app/calculators/page.tsx
+++ app/calculators/page.tsx
@@ -1,11 +1,13 @@
 import Main from '@/components/Main'
-// import {
-//   Card,
-//   CardDescription,
-//   CardHeader,
-//   CardTitle,
-// } from '@/components/ui/card'
-// import Link from 'next/link'
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import Link from 'next/link'
+
+const HIDE_CALCULATORS = true
 
 export default async function CalculatorsPage() {
   return (
@@ -13,23 +15,26 @@ export default async function CalculatorsPage() {
       <h1 className='text-6xl font-bold'>Calculators</h1>
 
       <div className='mt-10'>
-        <p>Calculators coming soon...</p>
-        {/* <div className='mt-4 grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3'>
-          <Link href={`/calculators/am-i-paying-too-much-for-rent`}>
-            <Card className='bg-white transition-shadow duration-500 hover:shadow-xl active:shadow-sm'>
-              <CardHeader>
-                <CardTitle className='text-lg'>
-                  Am I Paying Too Much for Rent?
-                </CardTitle>
-                <CardDescription>
-                  Half of Americans are paying too much for rent and don&apos;t
-                  even know it. Find out if you&apos;re paying too much and what
-                  your options are.
-                </CardDescription>
-              </CardHeader>
-            </Card>
-          </Link>
-        </div> */}
+        {HIDE_CALCULATORS ? (
+          <p>Calculators coming soon...</p>
+        ) : (
+          <div className='mt-4 grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3'>
+            <Link href={`/calculators/am-i-paying-too-much-for-rent`}>
+              <Card className='bg-white transition-shadow duration-500 hover:shadow-xl active:shadow-sm'>
+                <CardHeader>
+                  <CardTitle className='text-lg'>
+                    Am I Paying Too Much for Rent?
+                  </CardTitle>
+                  <CardDescription>
+                    Half of Americans are paying too much for rent and
+                    don&apos;t even know it. Find out if you&apos;re paying too
+                    much and what your options are.
+                  </CardDescription>
+                </CardHeader>
+              </Card>
+            </Link>
+          </div>
+        )}
       </div>
     </Main>
   )
